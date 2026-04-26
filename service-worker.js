59e18cf70bb164b92d82d4f96c28575956e5c450
MickeyJr12 <mdesroses@insa-toulouse.fr>
2026-04-26 18:41:10 +0200

    cache

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,19 @@
-const CACHE_NAME = 'geo-quiz-v1'
+const CACHE_NAME = 'worldmaps-v1';
 
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/icon3-GQ.png',
-  '/IconeOf-GQ.png',
-  '/maps-monde-home.png',
-]
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
 
-// Installation : on met tout en cache
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  )
-})
-
-// Activation : on supprime les vieux caches
-self.addEventListener('activate', e => {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
-  )
-})
+  );
+});
 
-// Fetch : cache d'abord, réseau ensuite
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  )
-})
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
