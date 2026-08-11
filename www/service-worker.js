3203ba408e08437316cea46ebfe5dd9373e6714a
MickeyJr12 <mdesroses@insa-toulouse.fr>
2026-08-11 01:48:52 -0400

    Ajout de Capacitor et de la plateforme iOS

---
+++ www/service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'worldmaps-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll([
+      '/',
+      '/index.html',
+    ]))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        // On ignore les réponses partielles (fonts, etc.)
+        if (response.status === 206) return response;
+        
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
